fa49e84e9f67c75e8c0ab89e17d57472e8fd2d21
ly4nn3 <july4nn3@gmail.com>
2025-01-12 00:15:45 +0800

    Exercise 2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,66 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradeNumber = int.Parse(grade);
+
+        string letter = "";
+
+        int remainder = gradeNumber % 10;
+
+        string sign = "";
+
+        if (gradeNumber >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradeNumber < 60)
+        {
+            letter = "F";
+        }
+
+        if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+
+        if (gradeNumber >= 93)
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congratulations! You've passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. I'm sure you'll do better next time");
+        }
     }
 }
