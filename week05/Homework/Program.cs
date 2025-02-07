76e85b280c35630b2934fb421f11bb4d1f6cf82b
ly4nn3 <july4nn3@gmail.com>
2025-02-07 22:06:01 +0800

    Week 5 Inheritance activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,17 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+    
+        Assignment assignment1 = new Assignment("Lyanne Lee", "CSE210");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment1 = new MathAssignment("Lyanne Lee", "Division", "5.3", "1-3");
+        Console.WriteLine(mathAssignment1.GetSummary());
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Lyanne Lee", "Korean History", "King Sejong");
+        Console.WriteLine(writingAssignment1.GetSummary());
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
     }
 }
