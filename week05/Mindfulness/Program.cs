eda35efd23ff0577eb3b63075c5a0e6fa06d2b95
ly4nn3 <july4nn3@gmail.com>
2025-02-09 03:25:41 +0800

    Added code for main program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        while (true)
+        {
+            try
+            {
+                Console.Clear();
+                
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Mindfulness Program Menu:");
+                Console.WriteLine("=========================");
+                Console.WriteLine("1. Start a breathing activity");
+                Console.WriteLine("2. Start a reflection activity");
+                Console.WriteLine("3. Start a listing activity");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("=========================");
+                Console.Write("Select a choice from the menu: ");
+                Console.ResetColor();
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        BreathingActivity breathingActivity = new BreathingActivity();
+                        breathingActivity.Run();
+                        break;
+                    case "2":
+                        ReflectionActivity reflectionActivity = new ReflectionActivity();
+                        reflectionActivity.Run();
+                        break;
+                    case "3":
+                        ListingActivity listingActivity = new ListingActivity();
+                        listingActivity.Run();
+                        break;
+                    case "4":
+                        Console.WriteLine("Thank you for using the Mindfulness Program. Goodbye!");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nAn error occured: {e.Message}");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
     }
 }
